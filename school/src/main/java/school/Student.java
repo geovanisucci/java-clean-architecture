348f30a55ea8734697bce37465b7c2d3f03c9aa4
geovanisucci <geovani@qyon.com.br>
2020-11-17 13:31:01 -0300

    indicator class implemented

--- school/src/main/java/school/Student.java
+++ school/src/main/java/school/Student.java
@@ -8,8 +8,14 @@ public class Student {
 	private Registration registrationNumber;
 	private String name;
 	private Email email;
-	private List<Phone> phones = new ArrayList<Phone>();
+	private List<Phone> phones = new ArrayList<>();
 	
+	public Student(Registration registrationNumber, String name, Email email) {		
+		this.registrationNumber = registrationNumber;
+		this.name = name;
+		this.email = email;
+	}
+
 	public void addPhone(String code, String number) {
 		this.phones.add(new Phone(code, number));
 	}
